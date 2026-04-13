36ee865810394681a89b49981a8b444c7d4df33e
Linda <lindakjellberg7@gmail.com>
2026-04-13 12:10:29 +0200

    Add response

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -6,5 +6,12 @@ document.querySelector("#loginForm")?.addEventListener("submit", async (e) => {
     const email = (document.querySelector("#email") as HTMLInputElement).value;
     const password = (document.querySelector("#password") as HTMLInputElement).value;
 
-    
+    const response = await fetch("http://localhost:3000login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+    });
 });

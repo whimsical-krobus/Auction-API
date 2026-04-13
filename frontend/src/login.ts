e7d70f5e1c2b3abb2f88c25532ef9fb4efb1321d
Linda <lindakjellberg7@gmail.com>
2026-04-13 12:07:28 +0200

    Add user variables from htma

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -2,5 +2,9 @@ import "./style.css";
 
 document.querySelector("#loginForm")?.addEventListener("submit", async (e) => {
     e.preventDefault();
+
+    const email = (document.querySelector("#email") as HTMLInputElement).value;
+    const password = (document.querySelector("#password") as HTMLInputElement).value;
+
     
 });

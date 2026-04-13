6aa43dfa5ad106bd7c571f71091dc80b95611a8c
Linda <lindakjellberg7@gmail.com>
2026-04-13 12:21:02 +0200

    Add message and status code

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -5,6 +5,7 @@ document.querySelector("#loginForm")?.addEventListener("submit", async (e) => {
 
     const email = (document.querySelector("#email") as HTMLInputElement).value;
     const password = (document.querySelector("#password") as HTMLInputElement).value;
+    const message = document.querySelector("#message") as HTMLDivElement;
 
     const response = await fetch("http://localhost:3000login", {
         method: "POST",
@@ -19,5 +20,8 @@ document.querySelector("#loginForm")?.addEventListener("submit", async (e) => {
         const user = (await response.json()) as { username: string; email: string; };
         sessionStorage.setItem("me", user.username);
         location.href = "/";
+    } else {
+        response.status === 401;
+        message.textContent = "Fel e-post eller lösenord.";
     }
 });

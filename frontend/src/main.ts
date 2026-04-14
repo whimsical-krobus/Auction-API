8e0fb8d287f0f65ddcdef9efc243811c91e5ab63
Farzad Sanaie <farzad@sanaie.se>
2026-04-14 17:37:20 +0200

    Added type for frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,7 @@
 import "./style.css";
 import { io } from "socket.io-client";
+import type { Auction } from "./models/Auction";
 
-type Auction = {
-  id: string;
-  title: string;
-  description: string;
-  imageUrl: string;
-  startingPrice: number;
-  currentPrice: number;
-  endTime: string;
-  createdBy: string;
-  leadingBidder: string;
-};
 
 const socket = io("http://localhost:3000", {
   withCredentials: true,
